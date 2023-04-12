70616da6697426dbd79f51f3b2c57fd8fc304174
Bed-com-30-19 <bed-com-30-19@unima.ac.mw>
2023-04-12 20:07:04 +0200

    manupulate and query objects from database

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const express = require('express')
 const app = express()
 
+const bodyparser = require('body-parser')
+
+//requesting body parsing Middleware
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: false}))
+
 const dbConnection = require('./src/utils/mysql.connector')
 
 const post = require('./src/posts/posts.model')
 
-app.get('/api/v1', function(req, res) {
-    return res.json(req.headers)
-})
+//app.get('/api/v1', function(req, res) {
+  //  return res.json(req.headers)
+//})
 
 app.get('/api/v1/posts', function(req, res) {
-    return res.json([post])
+    var sql2 = "SELECT * FROM posts*"
+    return dbConnection.query(sql2, function(error, results){
+        if(error) throw error;
+        return res.json(results)
+    })
 })
+
+//Create new post/article in the database
+app.post('/api/v1/posts', function(req, res){
+    //console.log(req.body)
+    const {name, imageurl,summery} = req.body//destructure sent properties grom the request body
+
+    //construct sql query
+    var sql = `INSERT INTO posts (name, imageUrl, summary) VALUES ('${name}','${imageUrl}','${summary}')`;
+    
+    //console.log(sql)
+    //Query the MySQL database and return results to the client app ie POSTMAN or web app
+    return dbConnection.query(sql, function(error, results){
+        if(error) throw error; 
+
+        return res.json(results)
+
+        //console.log("1 record inserted");
+    });
+    //res.json(sql)
+})
+
 
 app.listen(3000, function() {
     console.log('ROOMBOOKING listening on port 3000')
@@ -19,6 +50,6 @@ app.listen(3000, function() {
 
     dbConnection.connect(function(error) {
         if(error) throw error
-            console.log("MySQL Database is connected successfully!!!")
+            console.log("Connected to MySQL")
     })
 }) 
